4a93e7826eb5364e22c8615cc5799ecc36f5c076
DESKTOP-EAPE4TG\USER <joowon0410@naver.com>
2023-06-20 17:51:11 +0900

    신청, 투표 vue 껍데기 만들어놓기

---
+++ front/final_project/src/components/battle/application.vue
@@ -0,0 +1,28 @@
+<template>
+  theme : {{ theme }}
+  <input type="file">
+</template>
+
+<script>
+export default {
+  date(){
+    return{
+      theme:'',
+      memnum: sessionStorage.getItem("memnum"),
+      gender: ''
+    }
+  },
+  created: function(){
+    const self = this;
+  },
+  methods:{
+    applyBattle(){
+      this.$axios.post('http://localhost:8081')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
