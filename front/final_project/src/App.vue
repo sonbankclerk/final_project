1e6424c1b7996fad2f4ce6e823c62c4808b5f27c
DESKTOP-EAPE4TG\USER <joowon0410@naver.com>
2023-06-20 14:19:10 +0900

    app.vue 대충 변경 이건 저만 건들겠습니다.

--- front/final_project/src/App.vue
+++ front/final_project/src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/"><img src="./assets/logo.png" alt=""></router-link> 
+    <router-link to="/about">옷장</router-link> |
+    <router-link to="/about">커뮤니티</router-link> |
+    <router-link to="/about">명예의 전당</router-link> |
+    <span v-if="chkDay <= 3 && chkDay > 0"><router-link to="/about">배틀 투표</router-link> |</span>
+    <span v-else-if="chkDay == 4"><router-link to="/about">이 주의 우승자</router-link> |</span>
+    <span v-else><router-link to="/about">배틀 신청</router-link> |</span>
+    <router-link to="/about">로그인</router-link> |
+    <router-link to="/about">회원가입</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+export default{
+  data(){
+    return{
+    }
+  },
+  computed:{
+    chkDay(){
+      // 요일 별 배틀 변경.
+      let now = new Date();
+      return now.getDay();
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -13,6 +36,11 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+img{
+  width: 70px;
+  height: 70px;
 }
 
 nav {
