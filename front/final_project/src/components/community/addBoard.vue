71e541f154b7cfd29eb34739c9afa4adf26247eb
LAPTOP-SVMTQ420\mun75 <mun75056@gmail.com>
2023-06-22 17:54:10 +0900

    community 에서 add랑 폼 아직 미완성. 승미

---
+++ front/final_project/src/components/community/addBoard.vue
@@ -0,0 +1,23 @@
+<template >
+    <div>
+        <router-link to="/addBoard" >게시글 등록</router-link>
+        <br>
+        <input type="search" v-model="searchQuery" placeholder="#오피스룩 #패피">
+        <button @click="search">검색</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    },
+    method: {
+        
+    }
+}
+</script>
+<style>
+    
+</style>
